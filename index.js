922d6c0356b60355b528590eb1fd7b3457c8f286
BucurEva87 <bucur.eva87@gmail.com>
2022-07-18 19:52:42 +0300

    Modify JS file to abide linter's standards

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-function qs(selector = "*", element = document) {
+function qs(selector = '*', element = document) {
   return element.querySelector(selector);
 }
 
-function qsa(selector = "*", element = document) {
+function qsa(selector = '*', element = document) {
   return [...element.querySelectorAll(selector)];
 }
 
 const burger = qs('img[alt="hamburger"]');
 const burgerClose = qs('img[alt="hamburger close"]');
 
-qs(".hamburger").addEventListener("click", (e) => {
+qs('.hamburger').addEventListener('click', (e) => {
   let target = e.target;
-  if (!target.tagName.toLowerCase() == "img") return;
+  if (!target.tagName.toLowerCase() == 'img') return;
 
   toggleBurger();
 });
 
-qs("div.nav-links").addEventListener("click", (e) => {
+qs('div.nav-links').addEventListener('click', (e) => {
   if (window.innerWidth >= 768) return;
-  if (e.target.tagName.toLowerCase() != "a") return;
+  if (e.target.tagName.toLowerCase() != 'a') return;
 
   toggleBurger();
-  qs(e.target.getAttribute("href").replace("#", ".")).scrollIntoView();
+  qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
 });
 
 function toggleBurger() {
-  burger.classList.toggle("no-display");
-  burgerClose.classList.toggle("no-display");
-  qs("div.nav-links").classList.toggle("enlarged-menu");
+  burger.classList.toggle('no-display');
+  burgerClose.classList.toggle('no-display');
+  qs('div.nav-links').classList.toggle('enlarged-menu');
 }
 
 window.onresize = function () {
   if (
     window.innerWidth >= 768 &&
-    qs("div.nav-links").classList.contains("enlarged-menu")
+    qs('div.nav-links').classList.contains('enlarged-menu')
   ) {
     toggleBurger();
   }
 };
 
-for (link of qsa("div.nav-links a")) {
-  link.addEventListener("click", (e) => {
-    qs(e.target.getAttribute("href").replace("#", ".")).scrollIntoView();
+for (link of qsa('div.nav-links a')) {
+  link.addEventListener('click', (e) => {
+    qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
   });
 }
