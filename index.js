66a8f44af8e74153541bacdcaa8a06179318f96d
BucurEva87 <bucur.eva87@gmail.com>
2022-07-18 19:59:39 +0300

    Modify files to abide linter's standards

--- index.js
+++ index.js
@@ -6,40 +6,37 @@ function qsa(selector = '*', element = document) {
   return [...element.querySelectorAll(selector)];
 }
 
-const burger = qs('img[alt="hamburger"]');
-const burgerClose = qs('img[alt="hamburger close"]');
-
-qs('.hamburger').addEventListener('click', (e) => {
-  let target = e.target;
-  if (!target.tagName.toLowerCase() == 'img') return;
-
-  toggleBurger();
-});
-
-qs('div.nav-links').addEventListener('click', (e) => {
-  if (window.innerWidth >= 768) return;
-  if (e.target.tagName.toLowerCase() != 'a') return;
-
-  toggleBurger();
-  qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
-});
-
 function toggleBurger() {
   burger.classList.toggle('no-display');
   burgerClose.classList.toggle('no-display');
   qs('div.nav-links').classList.toggle('enlarged-menu');
 }
 
+const burger = qs('img[alt="hamburger"]');
+const burgerClose = qs('img[alt="hamburger close"]');
+
+qs('.hamburger').addEventListener('click', (e) => {
+  const target = e.target;
+  if (!target.tagName.toLowerCase() === 'img') return;
+
+  toggleBurger();
+});
+
+qs('div.nav-links').addEventListener('click', (e) => {
+  if (window.innerWidth >= 768) return;
+  if (e.target.tagName.toLowerCase() !== 'a') return;
+
+  toggleBurger();
+  qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
+});
+
 window.onresize = function () {
-  if (
-    window.innerWidth >= 768 &&
-    qs('div.nav-links').classList.contains('enlarged-menu')
-  ) {
+  if (window.innerWidth >= 768 && qs('div.nav-links').classList.contains('enlarged-menu')) {
     toggleBurger();
   }
 };
 
-for (link of qsa('div.nav-links a')) {
+for (let link of qsa('div.nav-links a')) {
   link.addEventListener('click', (e) => {
     qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
   });
