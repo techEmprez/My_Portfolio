c25502cf68d4e93322feff3deff756919eb98f4c
BucurEva87 <bucur.eva87@gmail.com>
2022-07-18 17:48:54 +0300

    Mobile menu

---
+++ index.js
@@ -0,0 +1,30 @@
+function qs(selector = '*', element = document) {
+  return element.querySelector(selector)
+}
+
+function qsa(selector = '*', element = document) {
+  return [...element.querySelectorAll(selector)]
+}
+
+const burger = qs('img[alt="hamburger"]')
+const burgerClose = qs('img[alt="hamburger close"]')
+
+qs('.hamburger').addEventListener('click', e => {
+  let target = e.target
+  if (!target.tagName.toLowerCase() == 'img') return
+
+  toggleBurger()
+})
+
+qs('div.nav-links').addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() != 'a') return
+
+  toggleBurger()
+  qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView()
+})
+
+function toggleBurger() {
+  burger.classList.toggle('no-display')
+  burgerClose.classList.toggle('no-display')
+  qs('div.nav-links').classList.toggle('enlarged-menu')
+}
