38e390a74e63dcda22d5c349de5af31717d9bc79
BucurEva87 <bucur.eva87@gmail.com>
2022-07-18 21:00:13 +0300

    Modify files to abide linter's standards

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 function qs(selector = '*', element = document) {
-  return element.querySelector(selector);
+  return element.querySelector(selector)
 }
 
 function qsa(selector = '*', element = document) {
-  return [...element.querySelectorAll(selector)];
+  return [...element.querySelectorAll(selector)]
 }
 
-const burger = qs('img[alt="hamburger"]');
-const burgerClose = qs('img[alt="hamburger close"]');
+const burger = qs('img[alt="hamburger"]')
+const burgerClose = qs('img[alt="hamburger close"]')
 
 function toggleBurger() {
-  burger.classList.toggle('no-display');
-  burgerClose.classList.toggle('no-display');
-  qs('div.nav-links').classList.toggle('enlarged-menu');
+  burger.classList.toggle('no-display')
+  burgerClose.classList.toggle('no-display')
+  qs('div.nav-links').classList.toggle('enlarged-menu')
 }
 
-qs('.hamburger').addEventListener('click', (e) => {
-  const { target } = e;
-  if (!target.tagName.toLowerCase() === 'img') return;
+qs('.hamburger').addEventListener('click', e => {
+  const { target } = e
+  if (!target.tagName.toLowerCase() === 'img') return
 
-  toggleBurger();
-});
+  toggleBurger()
+})
 
-qs('div.nav-links').addEventListener('click', (e) => {
-  if (window.innerWidth >= 768) return;
-  if (e.target.tagName.toLowerCase() !== 'a') return;
+qs('div.nav-links').addEventListener('click', e => {
+  if (window.innerWidth >= 768) return
+  if (e.target.tagName.toLowerCase() !== 'a') return
 
-  toggleBurger();
-  qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
-});
+  toggleBurger()
+  qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView()
+})
 
 window.onresize = () => {
   if (window.innerWidth >= 768 && qs('div.nav-links').classList.contains('enlarged-menu')) {
-    toggleBurger();
+    toggleBurger()
   }
 };
 
-for (const link of qsa('div.nav-links a')) {
-  link.addEventListener('click', (e) => {
-    qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView();
-  });
-}
+qsa('div.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    qs(e.target.getAttribute('href').replace('#', '.')).scrollIntoView()
+  })
+})
